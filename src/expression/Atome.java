b9a11e49f5322d14e3474240e2a676e820caf954
Pixselve <38262536+Pixselve@users.noreply.github.com>
2022-11-08 16:25:17 +0100

    feat: impl atome

--- src/expression/Atome.java
+++ src/expression/Atome.java
@@ -5,28 +5,32 @@ import java.util.Set;
 
 public class Atome extends Expression {
 
-	private String name;
+  private String name;
 
-	public Atome(String s) {
-		this.name = new String(s);
-	}
+  public Atome(String s) {
+    this.name = new String(s);
+  }
 
-	public boolean evalue() throws RuntimeException {
-		throw new RuntimeException("L'expression ne peut pas être évaluée car elle contient (au moins) l'atome "+name);
-	}
+  public boolean evalue() throws RuntimeException {
+    throw new RuntimeException("L'expression ne peut pas être évaluée car elle contient (au moins) l'atome " + name);
+  }
 
-	public Set<String> atomes() {
-		//TODO
-		return null;
-	}
+  public Set<String> atomes() {
+    Set<String> set = new HashSet<>();
+    set.add(this.name);
+    return set;
+  }
 
-	public Expression remplace(String s, boolean b) {
-		//TODO
-		return null;
-	}
+  public Expression remplace(String s, boolean b) {
+    if (this.name.equals(s)) {
+      return new Constante(b);
+    } else {
+      return this;
+    }
+  }
 
-	public Expression simplifier(){
-		return this;
-	}
+  public Expression simplifier() {
+    return this;
+  }
 
 }
